4705d2fbc1d0b94e5ec4b1d378932f8ef13e3022
BOUGET Alexandre <abouget68@gmail.com>
2023-10-21 19:04:51 +0200

    continue

--- src/include/kb_os.hpp
+++ src/include/kb_os.hpp
@@ -3,39 +3,20 @@
 
 /**
  * @brief OS family
- * @note 0: Unix, 1: Windows, 2: Mac
+ * @note 0: Linux, 1: Windows, 2: Mac
  */
 
-/**
- * @brief OS Name
- * @note Linux, Windows, Mac
- */
+
+#define KB_OS_LINUX 0
+#define KB_OS_WINDOWS 1
+#define KB_OS_MAC 2
 
 #ifdef _WIN32
-    #define KB_OS_FAMILY 1
-    #define KB_OS_NAME "Windows"
-    #ifdef _WIN64                   // 64 bits Windows
-    #else                           // 32 bits Windows
-    #endif
+    #define KB_OS_FAMILY KB_OS_WINDOWS
 #elif __APPLE__                     // Apple OS
-    #define KB_OS_FAMILY 2
-    #define KB_OS_NAME "Mac"
-    #include "TargetConditionals.h"
-    #ifdef TARGET_IPHONE_SIMULATOR     // iOS Simulator
-    #elif TARGET_OS_IPHONE          // iOS device
-    #elif TARGET_OS_MAC             // Other kinds of Mac OS
-    #else
-        #error "Unknown Apple platform"
-    #endif
-#elif __unix__                     // Unix
-    #define KB_OS_FAMILY 0
-    #define KB_OS_NAME "Unix"
-    #ifdef __linux__               // Linux
-        #define KB_OS_NAME "Linux"
-        #ifdef __ANDROID__         // Android
-            #define KB_OS_NAME "Android"
-        #endif
-    #endif
+    #define KB_OS_FAMILY KB_OS_MAC
+#elif __linux__
+    #define KB_OS_FAMILY KB_OS_LINUX
 #endif
 
 #endif
