6c00294bc968fc712a00535767aa654d4285b6da
BOUGET Alexandre <abouget68@gmail.com>
2024-04-06 23:35:38 +0200

    (#46) fix test

--- command/valgrind.sh
+++ command/valgrind.sh
@@ -4,4 +4,4 @@ ninja -C build
 
 valgrind --leak-check=full --show-leak-kinds=all -s \
           --track-origins=yes --log-file=valgrind-out.txt \
-          --suppressions=./valgrind ./build/Kromblast
+          --suppressions=./valgrind ./build/Kromblast json kromblast.json
